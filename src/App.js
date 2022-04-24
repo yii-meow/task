a24e7a1642928d431f15a6924c144034b934b92a
yiyi_meow <yiksoonchong2002@gmail.com>
2022-04-25 03:41:09 +0800

    tasks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks';
+import { useState } from "react"
 
-function App() {
+const App = () => {
+
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctors Appointment',
+      day: 'Feb 5th at 2:30pm',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Meeting at School',
+      day: 'Feb 6th at 1:40pm',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Food Shopping',
+      day: 'Feb 6th at 3:30pm',
+      reminder: false,
+    }
+  ]);
+
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  }
+
+  // Toggle Reminer
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
+  }
+
   return (
-    <div className="App">
-      <h1>Hi from yiyi &lt;3 </h1>
-      
+    <div className="container">
+      <Header />
+      {tasks.length > 0 ?
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> :
+        'No Tasks To Show'
+      }
     </div>
   );
 }
+
 
 export default App;
